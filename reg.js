72dc14fb568fd2ca0056e0b017c258933a2cac7e
DVShanina <97606151+DVShanina@users.noreply.github.com>
2022-08-09 10:03:02 +0400

    метод map(), const, строгое равенство

--- reg.js
+++ reg.js
@@ -1,35 +1,30 @@
 
-let users = [
+const users = [
     { name: 'leo', password: 'kisa', sex: 'man', },
     { name: 'ivan', password: 'lord', sex: 'man', },
- ];
+    ];
  
- ad_names = () => {
- let name = document.getElementById('name').value;
- let password = document.getElementById('password').value;
- let sex = document.getElementById('sex').value;
- let user = {};
- user.name=name;
- user.password=password;
- user.sex=sex;
- users.push(user);
- console.log(users); 
- return users;
- }
+    ad_names = () => {
+      const name = document.getElementById('name').value;
+      const password = document.getElementById('password').value;
+      const sex = document.getElementById('sex').value;
+      const user = {};
+      user.name=name;
+      user.password=password;
+      user.sex=sex;
+      users.push(user);
+      return users;
+      }
  
  function Input() {
     let login_ok = false;
-    let userName = document.getElementById("user_name").value
-    let userPassword = document.getElementById("user_password").value
+    let userName = document.getElementById("user_name").value.toLowerCase();
+    let userPassword = document.getElementById("user_password").value.toLowerCase();
  
-    userName = userName.toLowerCase();
-    userPassword = userPassword.toLowerCase();
- 
-    for (let i = 0; i < users.length-1; i++) {
-       if (userName==users[i].name && userPassword==users[i].password) {
-          login_ok = true;
-          alert("Добро пожаловать в VK!");
-       }
+      let usersMap = users.map ( (members) => {
+      if (members.name === userName && members.password === userPassword) {
+      login_ok = true;
+      alert("Добро пожаловать в VK!"); }} )
+      
+      if (login_ok == false) {alert("Неверный пароль или логин")}      
     }
-    if (login_ok == false) alert("Неверный логин или пароль!");
- }
